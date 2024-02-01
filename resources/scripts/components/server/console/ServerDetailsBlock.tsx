c7303dc91591f331f7b8f7670717ee44e23f6aea
unknown <woowujasonwu@gmail.com>
2024-02-01 12:57:00 +0800

    update server console

--- resources/scripts/components/server/console/ServerDetailsBlock.tsx
+++ resources/scripts/components/server/console/ServerDetailsBlock.tsx
@@ -127,11 +127,11 @@ const ServerDetailsBlock = ({ className }: { className?: string }) => {
             <StatBlock icon={faHdd} title={'Disk'} color={getBackgroundColor(stats.disk / 1024, limits.disk * 1024)}>
                 <Limit limit={textLimits.disk}>{bytesToString(stats.disk)}</Limit>
             </StatBlock>
-            <StatBlock icon={faCloudDownloadAlt} title={'Network (Inbound)'}>
-                {status === 'offline' ? <span className={'text-gray-400'}>Offline</span> : bytesToString(stats.rx)}
+            <StatBlock icon={faCloudDownloadAlt} title={'網路 (連入)'}>
+                {status === 'offline' ? <span className={'text-gray-400'}>離線</span> : bytesToString(stats.rx)}
             </StatBlock>
-            <StatBlock icon={faCloudUploadAlt} title={'Network (Outbound)'}>
-                {status === 'offline' ? <span className={'text-gray-400'}>Offline</span> : bytesToString(stats.tx)}
+            <StatBlock icon={faCloudUploadAlt} title={'網路 (連出)'}>
+                {status === 'offline' ? <span className={'text-gray-400'}>離線</span> : bytesToString(stats.tx)}
             </StatBlock>
         </div>
     );
