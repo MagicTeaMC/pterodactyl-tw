0d65502a03fd4d935514966c763eb7071445e895
Ryanisyyds <129717677+RyanisyydsTT@users.noreply.github.com>
2024-02-09 15:47:39 +0800

    Update advanced.blade.php

--- resources/views/admin/settings/advanced.blade.php
+++ resources/views/admin/settings/advanced.blade.php
@@ -2,14 +2,14 @@
 @include('partials/admin.settings.nav', ['activeTab' => 'advanced'])
 
 @section('title')
-    Advanced Settings
+    進階設定
 @endsection
 
 @section('content-header')
-    <h1>Advanced Settings<small>Configure advanced settings for Pterodactyl.</small></h1>
+    <h1>進階設定<small>為 Pterodactyl 配置進階設定。</small></h1>
     <ol class="breadcrumb">
-        <li><a href="{{ route('admin.index') }}">Admin</a></li>
-        <li class="active">Settings</li>
+        <li><a href="{{ route('admin.index') }}">管理員</a></li>
+        <li class="active">設定</li>
     </ol>
 @endsection
 
@@ -25,26 +25,26 @@
                     <div class="box-body">
                         <div class="row">
                             <div class="form-group col-md-4">
-                                <label class="control-label">Status</label>
+                                <label class="control-label">狀態</label>
                                 <div>
                                     <select class="form-control" name="recaptcha:enabled">
-                                        <option value="true">Enabled</option>
-                                        <option value="false" @if(old('recaptcha:enabled', config('recaptcha.enabled')) == '0') selected @endif>Disabled</option>
+                                        <option value="true">啟用</option>
+                                        <option value="false" @if(old('recaptcha:enabled', config('recaptcha.enabled')) == '0') selected @endif>停用</option>
                                     </select>
-                                    <p class="text-muted small">If enabled, login forms and password reset forms will do a silent captcha check and display a visible captcha if needed.</p>
+                                    <p class="text-muted small">如果啟用，登入表單和密碼重設表單將進行無聲驗證並在需要時顯示可見驗證。</p>
                                 </div>
                             </div>
                             <div class="form-group col-md-4">
-                                <label class="control-label">Site Key</label>
+                                <label class="control-label">站點金鑰</label>
                                 <div>
                                     <input type="text" required class="form-control" name="recaptcha:website_key" value="{{ old('recaptcha:website_key', config('recaptcha.website_key')) }}">
                                 </div>
                             </div>
                             <div class="form-group col-md-4">
-                                <label class="control-label">Secret Key</label>
+                                <label class="control-label">密鑰</label>
                                 <div>
                                     <input type="text" required class="form-control" name="recaptcha:secret_key" value="{{ old('recaptcha:secret_key', config('recaptcha.secret_key')) }}">
-                                    <p class="text-muted small">Used for communication between your site and Google. Be sure to keep it a secret.</p>
+                                    <p class="text-muted small">用於您的網站和 Google 之間的通信。請確保保密。</p>
                                 </div>
                             </div>
                         </div>
@@ -52,7 +52,7 @@
                             <div class="row">
                                 <div class="col-xs-12">
                                     <div class="alert alert-warning no-margin">
-                                        You are currently using reCAPTCHA keys that were shipped with this Panel. For improved security it is recommended to <a href="https://www.google.com/recaptcha/admin">generate new invisible reCAPTCHA keys</a> that tied specifically to your website.
+                                        您當前使用的是與此面板一起發送的 reCAPTCHA 金鑰。為了提高安全性，建議 <a href="https://www.google.com/recaptcha/admin">生成新的隱形 reCAPTCHA 金鑰</a>，專門綁定到您的網站。
                                     </div>
                                 </div>
                             </div>
@@ -61,22 +61,22 @@
                 </div>
                 <div class="box">
                     <div class="box-header with-border">
-                        <h3 class="box-title">HTTP Connections</h3>
+                        <h3 class="box-title">HTTP 連接</h3>
                     </div>
                     <div class="box-body">
                         <div class="row">
                             <div class="form-group col-md-6">
-                                <label class="control-label">Connection Timeout</label>
+                                <label class="control-label">連接逾時</label>
                                 <div>
                                     <input type="number" required class="form-control" name="pterodactyl:guzzle:connect_timeout" value="{{ old('pterodactyl:guzzle:connect_timeout', config('pterodactyl.guzzle.connect_timeout')) }}">
-                                    <p class="text-muted small">The amount of time in seconds to wait for a connection to be opened before throwing an error.</p>
+                                    <p class="text-muted small">在引發錯誤之前等待打開連接的時間量（以秒為單位）。</p>
                                 </div>
                             </div>
                             <div class="form-group col-md-6">
-                                <label class="control-label">Request Timeout</label>
+                                <label class="control-label">請求逾時</label>
                                 <div>
                                     <input type="number" required class="form-control" name="pterodactyl:guzzle:timeout" value="{{ old('pterodactyl:guzzle:timeout', config('pterodactyl.guzzle.timeout')) }}">
-                                    <p class="text-muted small">The amount of time in seconds to wait for a request to be completed before throwing an error.</p>
+                                    <p class="text-muted small">在引發錯誤之前等待完成請求的時間量（以秒為單位）。</p>
                                 </div>
                             </div>
                         </div>
@@ -84,44 +84,15 @@
                 </div>
                 <div class="box">
                     <div class="box-header with-border">
-                        <h3 class="box-title">Automatic Allocation Creation</h3>
+                        <h3 class="box-title">自動分配創建</h3>
                     </div>
                     <div class="box-body">
                         <div class="row">
                             <div class="form-group col-md-4">
-                                <label class="control-label">Status</label>
+                                <label class="control-label">狀態</label>
                                 <div>
                                     <select class="form-control" name="pterodactyl:client_features:allocations:enabled">
-                                        <option value="false">Disabled</option>
-                                        <option value="true" @if(old('pterodactyl:client_features:allocations:enabled', config('pterodactyl.client_features.allocations.enabled'))) selected @endif>Enabled</option>
+                                        <option value="false">停用</option>
+                                        <option value="true" @if(old('pterodactyl:client_features:allocations:enabled', config('pterodactyl.client_features.allocations.enabled'))) selected @endif>啟用</option>
                                     </select>
-                                    <p class="text-muted small">If enabled users will have the option to automatically create new allocations for their server via the frontend.</p>
-                                </div>
-                            </div>
-                            <div class="form-group col-md-4">
-                                <label class="control-label">Starting Port</label>
-                                <div>
-                                    <input type="number" class="form-control" name="pterodactyl:client_features:allocations:range_start" value="{{ old('pterodactyl:client_features:allocations:range_start', config('pterodactyl.client_features.allocations.range_start')) }}">
-                                    <p class="text-muted small">The starting port in the range that can be automatically allocated.</p>
-                                </div>
-                            </div>
-                            <div class="form-group col-md-4">
-                                <label class="control-label">Ending Port</label>
-                                <div>
-                                    <input type="number" class="form-control" name="pterodactyl:client_features:allocations:range_end" value="{{ old('pterodactyl:client_features:allocations:range_end', config('pterodactyl.client_features.allocations.range_end')) }}">
-                                    <p class="text-muted small">The ending port in the range that can be automatically allocated.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="box box-primary">
-                    <div class="box-footer">
-                        {{ csrf_field() }}
-                        <button type="submit" name="_method" value="PATCH" class="btn btn-sm btn-primary pull-right">Save</button>
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
-@endsection
+                                    <p
