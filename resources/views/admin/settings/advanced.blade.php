c6004ddda373421057dd6a3c77085e16b14d3e93
Ryanisyyds <129717677+RyanisyydsTT@users.noreply.github.com>
2024-02-10 12:24:16 +0800

    Update advanced.blade.php

--- resources/views/admin/settings/advanced.blade.php
+++ resources/views/admin/settings/advanced.blade.php
@@ -6,7 +6,7 @@
 @endsection
 
 @section('content-header')
-    <h1>進階設定<small>為 Pterodactyl 配置進階設定。</small></h1>
+    <h1>進階設定<small>為 Pterodactyl 設定進階設定。</small></h1>
     <ol class="breadcrumb">
         <li><a href="{{ route('admin.index') }}">管理員</a></li>
         <li class="active">設定</li>
@@ -61,12 +61,12 @@
                 </div>
                 <div class="box">
                     <div class="box-header with-border">
-                        <h3 class="box-title">HTTP 連接</h3>
+                        <h3 class="box-title">HTTP 連接線</h3>
                     </div>
                     <div class="box-body">
                         <div class="row">
                             <div class="form-group col-md-6">
-                                <label class="control-label">連接逾時</label>
+                                <label class="control-label">連線逾時</label>
                                 <div>
                                     <input type="number" required class="form-control" name="pterodactyl:guzzle:connect_timeout" value="{{ old('pterodactyl:guzzle:connect_timeout', config('pterodactyl.guzzle.connect_timeout')) }}">
                                     <p class="text-muted small">在引發錯誤之前等待打開連接的時間量（以秒為單位）。</p>
