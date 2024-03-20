805b6c023a0f2b12cf0e7a353e8b5d6d4a4bf305
Ryanisyyds <129717677+RyanisyydsTT@users.noreply.github.com>
2024-03-20 19:11:13 +0800

    Update index.blade.php

--- resources/views/admin/locations/index.blade.php
+++ resources/views/admin/locations/index.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.admin')
 
 @section('title')
-    Locations
+    位置
 @endsection
 
 @section('content-header')
-    <h1>Locations<small>All locations that nodes can be assigned to for easier categorization.</small></h1>
+    <h1>Locations<small>節點可以分配到的所有位置以便於分類。</small></h1>
     <ol class="breadcrumb">
         <li><a href="{{ route('admin.index') }}">Admin</a></li>
-        <li class="active">Locations</li>
+        <li class="active">位置</li>
     </ol>
 @endsection
 
@@ -17,7 +17,7 @@
     <div class="col-xs-12">
         <div class="box box-primary">
             <div class="box-header with-border">
-                <h3 class="box-title">Location List</h3>
+                <h3 class="box-title">位置列表</h3>
                 <div class="box-tools">
                     <button class="btn btn-sm btn-primary" data-toggle="modal" data-target="#newLocationModal">Create New</button>
                 </div>
@@ -27,10 +27,10 @@
                     <tbody>
                         <tr>
                             <th>ID</th>
-                            <th>Short Code</th>
-                            <th>Description</th>
-                            <th class="text-center">Nodes</th>
-                            <th class="text-center">Servers</th>
+                            <th>代碼</th>
+                            <th>描述</th>
+                            <th class="text-center">節點</th>
+                            <th class="text-center">伺服器</th>
                         </tr>
                         @foreach ($locations as $location)
                             <tr>
@@ -53,26 +53,26 @@
             <form action="{{ route('admin.locations') }}" method="POST">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title">Create Location</h4>
+                    <h4 class="modal-title">建立新位置</h4>
                 </div>
                 <div class="modal-body">
                     <div class="row">
                         <div class="col-md-12">
-                            <label for="pShortModal" class="form-label">Short Code</label>
+                            <label for="pShortModal" class="form-label">代碼</label>
                             <input type="text" name="short" id="pShortModal" class="form-control" />
-                            <p class="text-muted small">A short identifier used to distinguish this location from others. Must be between 1 and 60 characters, for example, <code>us.nyc.lvl3</code>.</p>
+                            <p class="text-muted small">用於區分此位置與其他位置的簡短標識。必須介於 1 到 60 個字元之間，例如 <code>us.nyc.lvl3</code></p>
                         </div>
                         <div class="col-md-12">
-                            <label for="pLongModal" class="form-label">Description</label>
+                            <label for="pLongModal" class="form-label">描述</label>
                             <textarea name="long" id="pLongModal" class="form-control" rows="4"></textarea>
-                            <p class="text-muted small">A longer description of this location. Must be less than 191 characters.</p>
+                            <p class="text-muted small">位置的詳細描述。必須少於 191 個字元。</p>
                         </div>
                     </div>
                 </div>
                 <div class="modal-footer">
                     {!! csrf_field() !!}
-                    <button type="button" class="btn btn-default btn-sm pull-left" data-dismiss="modal">Cancel</button>
-                    <button type="submit" class="btn btn-success btn-sm">Create</button>
+                    <button type="button" class="btn btn-default btn-sm pull-left" data-dismiss="modal">取消</button>
+                    <button type="submit" class="btn btn-success btn-sm">建立</button>
                 </div>
             </form>
         </div>
